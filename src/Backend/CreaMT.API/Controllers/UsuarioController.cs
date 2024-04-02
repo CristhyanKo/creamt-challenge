87975f51c87663e2809c747ada9d61fbbb8c0f61
Douglasdsm <douglassilvamartinsnegocios@hotmail.com>
2024-04-02 12:28:30 -0400

    feat: adicionando endpoint para registrar usuário

---
+++ src/Backend/CreaMT.API/Controllers/UsuarioController.cs
@@ -0,0 +1,17 @@
+﻿using CreaMT.Communication.Requests;
+using CreaMT.Communication.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CreaMT.API.Controllers;
+[Route("[controller]")]
+[ApiController]
+public class UsuarioController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseRegisteredUsuariosJson),StatusCodes.Status201Created)]
+    public IActionResult Register(RequestRegisterUsuarioJson request)
+    { 
+        return Created();
+    }
+}
---
+++ src/Shared/CreaMT.Communication/Responses/ResponseRegisteredUsuariosJson.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreaMT.Communication.Responses;
+public class ResponseRegisteredUsuariosJson
+{
+    public string Nome { get; set; } = string.Empty;
+}
