f5a7f3b0d4cd07f2950fec7be2387e3faaaa01d3
Douglasdsm <douglassilvamartinsnegocios@hotmail.com>
2024-04-03 09:20:27 -0400

    Criando entidade usuário

---
+++ src/Backend/CreaMT.Domain/Entities/EntityBase.cs
@@ -0,0 +1,9 @@
+﻿namespace CreaMT.Domain.Entities;
+public class EntityBase
+{
+    public long Id { get; set; }
+    public DateTime DataCadastro { get; set; } = DateTime.UtcNow;
+    public DateTime? DataAtualizacao { get; set; } = null;
+    public bool Ativo { get; set; } = true;
+    public bool Excluido { get; set; } = false;
+}
