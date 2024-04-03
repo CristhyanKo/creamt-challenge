914da89f7c0d40786acacecccc0e51f656964c04
Douglasdsm <douglassilvamartinsnegocios@hotmail.com>
2024-04-03 09:22:25 -0400

    Criando classe Repositorio do usuário e contexto do banco de dados

---
+++ src/Backend/CreaMT.infrastructure/DataAcess/CreaMTAPIDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CreaMT.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CreaMT.infrastructure.DataAcess;
+public class CreaMTAPIDbContext : DbContext
+{
+    public CreaMTAPIDbContext(DbContextOptions options) : base(options) { }
+
+    public DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(CreaMTAPIDbContext).Assembly);
+    }
+}
---
+++ src/Backend/CreaMT.infrastructure/DataAcess/Repositories/UsuarioRepository.cs
@@ -0,0 +1,24 @@
+﻿using CreaMT.Domain.Entities;
+using CreaMT.Domain.Repositories.Usuario;
+using Microsoft.EntityFrameworkCore;
+
+namespace CreaMT.infrastructure.DataAcess.Repositories;
+public class UsuarioRepository : IUsuarioReadOnlyRepository, IUsuarioWriteOnlyRepository
+{
+    private readonly CreaMTAPIDbContext _dbContext;
+
+    public UsuarioRepository(CreaMTAPIDbContext dbContext) => _dbContext = dbContext;
+
+    public async Task Add(Usuario usuario) => await _dbContext.Usuarios.AddAsync(usuario);
+
+    public async Task<bool> ExistActiveUsuarioWithEmail(string email)
+    {
+        return await _dbContext.Usuarios.AnyAsync(u => u.Email == email && u.Ativo && u.Excluido == false);
+    }
+
+    public async Task<bool> ExistActiveUsuarioWithCpfCnpj(string CpfCnpj)
+    {
+        return await _dbContext.Usuarios.AnyAsync(u => u.CpfCnpj == CpfCnpj && u.Ativo && u.Excluido == false);
+    }
+
+}
