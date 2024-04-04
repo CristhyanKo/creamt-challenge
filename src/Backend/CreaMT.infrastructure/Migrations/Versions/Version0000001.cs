a608197c4d4005358c3d697a76aa41e1d762f51e
Douglasdsm <douglassilvamartinsnegocios@hotmail.com>
2024-04-04 11:03:09 -0400

    Criação da tabela de Usuario para o banco de dados usando FluentMigration

---
+++ src/Backend/CreaMT.infrastructure/Migrations/Versions/Version0000001.cs
@@ -0,0 +1,17 @@
+﻿using CreaMT.infrastructure.Migrations;
+using FluentMigrator;
+
+namespace ApiCreaMT.Infrastructure.Migrations.Versoes;
+[Migration(DatabaseVersions.TABLE_USUARIO, "Cria tabela usuario")]
+public class Versao0000001 : VersionBase
+{
+    public override void Up()
+    {
+        CreateTable("Usuarios")
+            .WithColumn("Nome").AsString(100).NotNullable()
+            .WithColumn("Email").AsString(255).NotNullable()
+            .WithColumn("Telefone").AsString(11).NotNullable()
+            .WithColumn("CpfCnpj").AsString(14).NotNullable()
+            .WithColumn("Senha").AsString(2000).NotNullable();
+    }
+}
