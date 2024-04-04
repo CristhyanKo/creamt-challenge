475f14088a5edf74a0b5c4b296b17a4bacea2071
Douglasdsm <douglassilvamartinsnegocios@hotmail.com>
2024-04-04 11:05:07 -0400

    Criação da classe responsavel por criar banco de dados se o mesmo não existir e tambem fazer uso das migratios no caso criar as tabelas.

---
+++ src/Backend/CreaMT.infrastructure/Migrations/DatabaseMigration.cs
@@ -0,0 +1,41 @@
+﻿using Dapper;
+using FluentMigrator;
+using FluentMigrator.Runner;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.DependencyInjection;
+using CreaMT.Domain.Extensions;
+
+namespace CreaMT.infrastructure.Migrations;
+public static class DatabaseMigration
+{
+    public static void Migrat(string connectionString, IServiceProvider serviceProvider)
+    {
+        EnsureDatabaseCreate_SqlServer(connectionString);
+        MigrationDatabase(serviceProvider);
+    }
+
+
+    private static void EnsureDatabaseCreate_SqlServer(string connectionString)
+    {
+        var connectionStringBuilder = new SqlConnectionStringBuilder(connectionString);
+        var databaseName = connectionStringBuilder.InitialCatalog;
+        connectionStringBuilder.Remove("Database");
+        using var dbConnection = new SqlConnection(connectionStringBuilder.ConnectionString);
+
+        var parameters = new DynamicParameters();
+        parameters.Add("databaseName", databaseName);
+
+        var records = dbConnection.Query("SELECT * FROM sys.databases WHERE name = @databaseName", parameters);
+        if(records.Any().IsFalse())
+        {
+            dbConnection.Execute($"CREATE DATABASE {databaseName}");
+        }
+    }
+
+    private static void MigrationDatabase(IServiceProvider serviceProvider)
+    {
+        var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+        runner.ListMigrations();
+        runner.MigrateUp();
+    }
+}
