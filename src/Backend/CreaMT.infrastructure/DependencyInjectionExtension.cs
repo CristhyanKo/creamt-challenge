5042091e45ecc4f16d5492a7ff2753ed9b40b2e8
Douglasdsm <douglassilvamartinsnegocios@hotmail.com>
2024-04-04 11:08:53 -0400

    injeção de dependencia do projeto infrastructure

--- src/Backend/CreaMT.infrastructure/DependencyInjectionExtension.cs
+++ src/Backend/CreaMT.infrastructure/DependencyInjectionExtension.cs
@@ -2,26 +2,31 @@
 using CreaMT.Domain.Repositories.Usuario;
 using CreaMT.infrastructure.DataAcess;
 using CreaMT.infrastructure.DataAcess.Repositories;
+using CreaMT.infrastructure.Extension;
+using FluentMigrator.Runner;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace CreaMT.infrastructure;
 public static class DependencyInjectionExtension
 {
     public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        AddDbContext(services, configuration);
         AddRepositories(services);
+        if (configuration.IsUnitTestEnviroment())
+            return;
+ 
+        AddFluentMigrator(services, configuration);
+        AddDbContext(services, configuration);
     }
 
     private static void AddDbContext(IServiceCollection services, IConfiguration configuration)
     {
-        var connectionString = configuration.GetConnectionString("Conexao");
         services.AddDbContext<CreaMTAPIDbContext>(dbContextOptions =>
         {
-            //dbContextOptions.UseSqlServer(configuration.GetConnectionString("CreaMTAPIDbContext"));
-            dbContextOptions.UseSqlServer(connectionString);
+            dbContextOptions.UseSqlServer(configuration.GetConexaoCompleta());
         });
     }
 
@@ -30,5 +35,16 @@ public static class DependencyInjectionExtension
         services.AddScoped<IUnitOfWork, UnitOfWork>();
         services.AddScoped<IUsuarioReadOnlyRepository, UsuarioRepository>();
         services.AddScoped<IUsuarioWriteOnlyRepository, UsuarioRepository>();
+    }
+
+    private static void AddFluentMigrator(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddFluentMigratorCore().ConfigureRunner(options => {
+            options
+            .AddSqlServer()
+            .WithGlobalConnectionString(configuration.GetConexaoCompleta())
+            .ScanIn(Assembly.Load("CreaMT.infrastructure")).For.All();
+
+        });
     }
 }
