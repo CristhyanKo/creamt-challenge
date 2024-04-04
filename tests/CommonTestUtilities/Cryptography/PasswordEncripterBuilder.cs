7ffe5a6d036d6a8e9bf63f973c5095891fede380
Douglasdsm <douglassilvamartinsnegocios@hotmail.com>
2024-04-04 11:10:39 -0400

    classe para criptografar senha

---
+++ tests/CommonTestUtilities/Cryptography/PasswordEncripterBuilder.cs
@@ -0,0 +1,7 @@
+﻿using CreaMT.Application.Services.Cryptography;
+
+namespace CleaMT.CommonTestUtilities.Cryptography;
+public class PasswordEncripterBuilder
+{
+    public static PasswordEncripter Build() => new PasswordEncripter("abc1234");
+}
