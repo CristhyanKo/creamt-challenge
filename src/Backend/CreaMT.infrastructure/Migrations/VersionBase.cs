9706faa8ebfe6e5c68c5bbcd0b3bb2f19327e881
Douglasdsm <douglassilvamartinsnegocios@hotmail.com>
2024-04-04 11:07:00 -0400

    classe que contem todas as colunas comum nas tabelas

---
+++ src/Backend/CreaMT.infrastructure/Migrations/VersionBase.cs
@@ -0,0 +1,16 @@
+﻿using FluentMigrator;
+using FluentMigrator.Builders.Create.Table;
+
+namespace ApiCreaMT.Infrastructure.Migrations;
+public abstract class VersionBase : ForwardOnlyMigration
+{
+    protected ICreateTableColumnOptionOrWithColumnSyntax CreateTable(string table)
+    {
+        return Create.Table(table)
+                .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+                .WithColumn("DataCadastro").AsDateTime().NotNullable()
+                .WithColumn("DataAtualizacao").AsDateTime().Nullable()
+                .WithColumn("Ativo").AsBoolean().NotNullable()
+                .WithColumn("Excluido").AsBoolean().NotNullable();
+    }
+}
