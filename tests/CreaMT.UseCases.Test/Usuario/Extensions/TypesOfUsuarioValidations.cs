5db21ef592015effb6eaca5d1985496bcdac405e
Douglasdsm <douglassilvamartinsnegocios@hotmail.com>
2024-04-04 11:12:33 -0400

    Projeto de teste com casos de uso do Usuário do cadastro de usuários ate o momento

---
+++ tests/CreaMT.UseCases.Test/Usuario/Extensions/TypesOfUsuarioValidations.cs
@@ -0,0 +1,7 @@
+﻿namespace CreaMT.UseCases.Test.Usuario.Extensions;
+public enum TypesOfUsuarioValidations
+{
+    RepositoryDataEmail = 1,
+    RepositoryDataCpfCnpj = 2,
+    RepositoryWithoutConsultation = 3
+}
