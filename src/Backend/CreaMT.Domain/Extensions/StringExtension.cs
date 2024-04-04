1042da18326c1cb4fbc1758ab153960d4723f20e
Douglasdsm <douglassilvamartinsnegocios@hotmail.com>
2024-04-04 15:22:27 -0400

    Adicionando remover mascara extensões string para não duplicar codigo

--- src/Backend/CreaMT.Domain/Extensions/StringExtension.cs
+++ src/Backend/CreaMT.Domain/Extensions/StringExtension.cs
@@ -1,7 +1,11 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
 
 namespace CreaMT.Domain.Extensions;
 public static class StringExtension
 {
     public static bool NotEmpty([NotNullWhen(true)] this string? value) => string.IsNullOrWhiteSpace(value).IsFalse();
+
+    public static string RemoveMascara(this string value) => Regex.Replace(value, "[^0-9]", "");
+
 }
