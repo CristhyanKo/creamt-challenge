a8e07df4d1e3f06b1ead4b74c10af97eac0ea150
Douglasdsm <douglassilvamartinsnegocios@hotmail.com>
2024-04-05 15:41:29 -0400

    Criando Migration Documento

---
+++ src/Backend/CreaMT.infrastructure/Migrations/Versions/Version0000004.cs
@@ -0,0 +1,17 @@
+﻿using ApiCreaMT.Infrastructure.Migrations;
+using FluentMigrator;
+
+namespace CreaMT.infrastructure.Migrations.Versions;
+[Migration(DatabaseVersions.TABLE_DOCUMENTO, "Cria tabela documento")]
+public class Version0000004 : VersionBase
+{
+    public override void Up()
+    {
+        CreateTable("Documentos")
+            .WithColumn("Nome").AsString(100).NotNullable()
+            .WithColumn("EnderecoArquivo").AsString(300).NotNullable()
+            .WithColumn("Downloads").AsInt32().NotNullable()
+            .WithColumn("ServicoId").AsInt64().NotNullable().ForeignKey("FK_Documento_Servico_Id", "Servicos", "Id")
+            .WithColumn("UsuarioId").AsInt64().NotNullable().ForeignKey("FK_Documento_Usuario_Id", "Usuarios", "Id");
+    }
+}
