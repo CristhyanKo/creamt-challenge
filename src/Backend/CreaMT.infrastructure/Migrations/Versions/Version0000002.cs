4ea5f0fe4add69bef5ba882ed4f825fb5dba1c5c
Douglasdsm <douglassilvamartinsnegocios@hotmail.com>
2024-04-05 15:40:54 -0400

    correção de nome de restrição  chave estrangueira

--- src/Backend/CreaMT.infrastructure/Migrations/Versions/Version0000002.cs
+++ src/Backend/CreaMT.infrastructure/Migrations/Versions/Version0000002.cs
@@ -1,10 +1,5 @@
 ﻿using ApiCreaMT.Infrastructure.Migrations;
 using FluentMigrator;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CreaMT.infrastructure.Migrations.Versions;
 [Migration(DatabaseVersions.TABLE_CLIENTE, "Cria tabela cliente")]
@@ -18,6 +13,6 @@ public class Version0000002 : VersionBase
             .WithColumn("Telefone").AsString(11).NotNullable()
             .WithColumn("CpfCnpj").AsString(14).NotNullable()
             .WithColumn("Anuidade_Paga").AsBoolean().NotNullable()
-            .WithColumn("UsuarioId").AsInt64().NotNullable().ForeignKey("FK_Usuario_Cliente_Id", "Usuarios","Id");
+            .WithColumn("UsuarioId").AsInt64().NotNullable().ForeignKey("FK_Cliente_Usuario_Id", "Usuarios","Id");
     }
 }
