b1762b2f71b0240b9f0e2b119fe99edd06a976ca
Douglasdsm <douglassilvamartinsnegocios@hotmail.com>
2024-04-05 16:20:45 -0400

    Criação da entidade solicitação documentos

---
+++ src/Backend/CreaMT.Domain/Entities/SolicitacoesDocumentos.cs
@@ -0,0 +1,9 @@
+﻿namespace CreaMT.Domain.Entities;
+public class SolicitacaoDocumento : EntityBase
+{
+    public long SolicitacaoId { get; set; }
+    public long DocumentoId { get; set; }
+    public long UsuarioId { get; set; }
+    public int Downloads { get; set; }
+    public Documento Documento { get; set; }
+}
