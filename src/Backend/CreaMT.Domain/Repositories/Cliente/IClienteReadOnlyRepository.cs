10e2bbbcea65985a6c881a8490b21de871e739c6
Douglasdsm <douglassilvamartinsnegocios@hotmail.com>
2024-04-05 20:11:40 -0400

    implementação do metodo getAll  em Cliente e Documento e na interface de servicos

---
+++ src/Backend/CreaMT.Domain/Repositories/Cliente/IClienteReadOnlyRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace CreaMT.Domain.Repositories.Cliente;
+public interface IClienteReadOnlyRepository
+{
+    public Task<bool> ExistActiveClienteWithCpfCnpj(string email);
+    public Task<bool> ExistActiveClienteWithEmail(string CpfCnpj);
+    public Task<IList<Entities.Cliente>> GetAll();
+
+}
